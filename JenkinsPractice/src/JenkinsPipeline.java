3f0badfe0d48da18f7c4cf861135cccaae01e2e3
krupa <krupa@krupa-OEM>
2019-02-20 09:48:36 +0530

    Git Pust Test

---
+++ JenkinsPractice/src/JenkinsPipeline.java
@@ -0,0 +1,12 @@
+
+public class JenkinsPipeline {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Learn Jenkins Pipeline");
+		
+		System.out.print("Eclipse is configured with Git");
+
+	}
+
+}
