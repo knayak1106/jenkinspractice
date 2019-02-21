7f90078de0b59548330dee682cba6b589240d811
krupa <knayak1106@gmail.com>
2019-02-21 09:15:07 +0530

    Commit and Push working fine from Terminal

--- JenkinsPractice/src/JenkinsPipeline.java
+++ JenkinsPractice/src/JenkinsPipeline.java
@@ -9,6 +9,7 @@ public class JenkinsPipeline {
 		System.out.print("Eclipse is connected with GitHub");
 		System.out.print("Unable to commit and push");
 		System.out.print("Git push worked but by force");
+		System.out.print("Git Commit and Push now worked from Terminal");
 
 	}
 
